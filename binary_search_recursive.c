372f7cd760d8a52048b216923cc511b121b09140
Shivajidhamale <45144598+Shivajidhamale@users.noreply.github.com>
2018-12-23 10:39:52 +0530

    Update binary_search_recursive.c

--- binary_search_recursive.c
+++ binary_search_recursive.c
@@ -3,19 +3,19 @@
 int binary_search(int arr[], int start, int end, int s_key)
 {
 
-        while( start <= end)
-        {
-                int mid = (start + end)/2;
-                if(arr[mid] == s_key)
-                        return mid;
+	if(start <= end)
+	{
+		int mid = (start + end)/2;
+		if(arr[mid] == s_key)
+			return mid;
 
-                if(s_key < arr[mid] )
-                         return binary_search(arr,start, mid -1 , s_key);
-                else
-                        return binary_search(arr, mid+1, end, s_key);
-        }
+		if(s_key < arr[mid] )
+			 return binary_search(arr,start, mid-1, s_key);
+		else
+                	 return binary_search(arr, mid+1, end, s_key);
+	}
 
-        return -1;
+	return -1;
 }
 int main()
 {
@@ -23,9 +23,9 @@ int main()
     int index;
     int ar[] = {1,10,11,15,17,18,19,20,100};
 
-    //int ar[] = { 3, 1, 4};
+    //int ar[] = { 3, 1, 4}; 
     int end = sizeof(ar)/sizeof(int);
-    index = binary_search(ar, 0, end-1, 20);
+    index = binary_search(ar, 0, end-1, 19);
      printf("\n%d",index);
     printf("\n\n");
     return 0;
